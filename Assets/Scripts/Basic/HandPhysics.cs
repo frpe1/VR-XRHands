bd413af2aa7b66ce2b5a82daeb3996243d7e0385
Fredrik <61582421+frpe1@users.noreply.github.com>
2020-09-22 22:37:59 +0200

    setting the hands scripts basics

--- Assets/Scripts/Basic/HandPhysics.cs
+++ Assets/Scripts/Basic/HandPhysics.cs
@@ -3,48 +3,72 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandPhysics : MonoBehaviour
 {
+    public float smoothingAmount = 15.0f;
+    public Transform target;
+
+    private Vector3 targetPosition = Vector3.zero;
+    private Quaternion targetRotation = Quaternion.identity;
+
+    private Rigidbody rigidBody = null;
+
     private void Awake()
     {
-
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     private void Start()
     {
-
+        // när skriptet körs, så ser det här till att handen är exakt där det ska vara från start
+        TeleportToTarget();
     }
 
     private void Update()
     {
-
+        SetTargetPosition();
+        SetTargetRotation();
     }
 
     private void SetTargetPosition()
     {
+        float time = smoothingAmount * Time.unscaledDeltaTime;
+        targetPosition = Vector3.Lerp(targetPosition, target.position, time);
 
     }
 
     private void SetTargetRotation()
     {
+        // Det här är vart vi är nu och vart vi ska/vill göra, och detta avgör hur många steg vi kan gå åt det hållet
 
+        float time = smoothingAmount * Time.unscaledDeltaTime;
+        targetRotation = Quaternion.Slerp(targetRotation, target.rotation, time);
     }
 
     private void FixedUpdate()
     {
-
+        // Eftersom vi har en rigidbody så måste vi köra fixedupdate
+        MoveToController();
+        RotateToController();
     }
 
     private void MoveToController()
     {
-
+        Vector3 positionDelta = targetPosition - transform.position;
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.MovePosition(transform.position + positionDelta);
     }
 
     private void RotateToController()
     {
-
+        rigidBody.angularVelocity = Vector3.zero;
+        rigidBody.MovePosition(targetPosition);
     }
 
     public void TeleportToTarget()
     {
+        targetPosition = target.position;
+        targetRotation = target.rotation;
 
+        transform.position = targetPosition;
+        transform.rotation = targetRotation;
     }
 }
